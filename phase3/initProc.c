9964be0c8333d3d2d85b07cdd510202ca0bb3978
Dang Truong <truongminhdang26403@gmail.com>
2025-04-02 11:24:05 -0400

    fix: fix the kernel panic bug when accessing the page of  process

--- phase3/initProc.c
+++ phase3/initProc.c
@@ -19,10 +19,6 @@
 int masterSemaphore;              /* Master semaphore for termination */
 int supportDeviceSem[NUMDEVICES]; /* support level device semaphore */
 
-void debug(int a, int b, int c, int d) {
-
-}
-
 /* Initialize a U-proc's processor state */
 HIDDEN void initUProcState(state_t *state, int asid) {
   state->s_pc = state->s_t9 = UPROC_PC;
@@ -41,10 +37,12 @@ HIDDEN void initPageTable(support_t *sup, int asid) {
     programTrapHandler(sup);
   }
 
-  /* Extract the .text memory size from the header. The header field for .text Memory Size is at offset 0x000C. */
+  /* Extract the .text memory size from the header. The header field for .text
+   * Memory Size is at offset 0x000C. */
   unsigned int textMemSize = *(unsigned int *)(headerBuf + 0x000C);
 
-  /* Compute the number of pages required for the .text section. Rounding up if necessary */
+  /* Compute the number of pages required for the .text section. Rounding up if
+   * necessary */
   int textPages = (textMemSize + PAGESIZE - 1) / PAGESIZE;
 
   /* Initialize the first 31 entries (text and data pages) */
@@ -73,17 +71,25 @@ HIDDEN void initSupportStruct(support_t *sup, int asid) {
   /* Set ASID for the process */
   sup->sup_asid = asid;
 
+  /* Determine RAMTOP */
+  devregarea_t *busRegArea = (devregarea_t *)RAMBASEADDR;
+  memaddr RAMTOP = RAMSTART + busRegArea->ramsize;
+
+  /* Avoiding the last frame: Reserve the last frame below RAMTOP for the test
+   * process */
+  memaddr SUPPORT_STACK_BASE = RAMTOP - (asid * PAGESIZE * 2);
+
   /* TLB exception context */
   context_t *excCtxTLB = &sup->sup_exceptContext[PGFAULTEXCEPT];
   excCtxTLB->c_pc = (memaddr)uTLB_ExceptionHandler;
   excCtxTLB->c_status = STATUS_IEP | STATUS_IM_ALL_ON | STATUS_TE;
-  excCtxTLB->c_stackPtr = (memaddr)&sup->sup_stackTLB[499];
+  excCtxTLB->c_stackPtr = SUPPORT_STACK_BASE;
 
   /* General exception context */
   context_t *excCtxGen = &sup->sup_exceptContext[GENERALEXCEPT];
   excCtxGen->c_pc = (memaddr)supportExceptionHandler;
   excCtxGen->c_status = STATUS_IEP | STATUS_IM_ALL_ON | STATUS_TE;
-  excCtxGen->c_stackPtr = (memaddr)&sup->sup_stackGen[499];
+  excCtxGen->c_stackPtr = SUPPORT_STACK_BASE - PAGESIZE;
 
   initPageTable(sup, asid);
 }
