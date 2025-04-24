fd6a396965301bdb34c8dba2e9dd28d1380a5080
Dang Truong <truongminhdang26403@gmail.com>
2025-04-23 21:09:07 -0400

    feat(phase6): add skeleton code and test files for phase 6

---
+++ h/alsl.h
@@ -0,0 +1,20 @@
+#ifndef ALSL
+#define ALSL
+
+/**
+ * @file alsl.h
+ * @author Dang Truong
+ * @brief The externals declaration file for the Active Logical Semaphore List
+ * (ALSL)
+ * @date 2025-04-23
+ *
+ * @copyright Copyright (c) 2025
+ *
+ */
+
+#include "../h/types.h"
+
+void sysPasserenLogicalSem(state_t *excState, support_t *sup);
+void sysVerhogenLogicalSem(state_t *excState, support_t *sup);
+
+#endif
---
+++ phase6/alsl.c
@@ -0,0 +1,10 @@
+/**
+ * @file alsl.c
+ * @author Dang Truong
+ * @brief Implement the Active Logical Semaphore List (ALSL)
+ * @date 2025-04-23
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+
